5a81e4800185820dedbcaeeb23304251dbce4895
lakshyagurjar20 <lakshygurjar2095@gmail.com>
2025-06-27 16:17:06 +0530

    Add create task form, view, and template

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('add/', views.add_task, name='add_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,3 +4,16 @@ from .models import Task
 def task_list(request):
     tasks = Task.objects.all().order_by('-created')
     return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+from django.shortcuts import redirect
+from .forms import TaskForm
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'todo/add_task.html', {'form': form})
