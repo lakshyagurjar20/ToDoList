8a183b5c37873b9abf1ee7cd3cbf4d970d79d819
lakshyagurjar20 <lakshygurjar2095@gmail.com>
2025-06-27 16:25:07 +0530

    Add update task view, template, and links

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
+    path('update/<int:pk>/', views.update_task, name='update_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -17,3 +17,17 @@ def add_task(request):
     else:
         form = TaskForm()
     return render(request, 'todo/add_task.html', {'form': form})
+
+from django.shortcuts import get_object_or_404
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/update_task.html', {'form': form})
+
